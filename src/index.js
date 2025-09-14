52e9114cdf3ceb196941d5f7ba9dba63615f1bf2
Anatoli33 <hadjiev1215@gmail.com>
2025-09-14 19:31:33 +0300

    Home HTML connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import http from 'http';
+import fs from 'fs/promises';
 
 const port = 1337;
-const serve = http.createServer((req, res) =>{
-    res.writeHead(200, {
-        'Content-Type': "text/plain"
-        });
-        res.write('Hello World!');
-        res.end();
-})
-serve.listen(port);
+
+const serve = http.createServer(async (req, res) => {
+  try {
+    if (req.url === "/") {
+      const homeHTML = await fs.readFile('./src/views/home/index.html', { encoding: 'utf-8' });
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(homeHTML);
+
+    } else if (req.url === "/styles/site.css") {
+      const siteCss = await fs.readFile('./src/styles/site.css', { encoding: 'utf-8' });
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.write(siteCss);
+
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write("404 Not Found");
+    }
+  } catch (err) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.write("Server Error: " + err.message);
+  }
+  res.end();
+});
+
+serve.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
