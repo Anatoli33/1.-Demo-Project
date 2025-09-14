13f685c88a75eed635b8653e266d53c0079f5d65
Anatoli33 <hadjiev1215@gmail.com>
2025-09-14 19:57:13 +0300

    Adding more views

--- src/index.js
+++ src/index.js
@@ -5,24 +5,43 @@ const port = 1337;
 
 const serve = http.createServer(async (req, res) => {
   try {
-    if (req.url === "/") {
-      const homeHTML = await fs.readFile('./src/views/home/index.html', { encoding: 'utf-8' });
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(homeHTML);
+    switch (req.url) {
+      case "/": {
+        const homeHTML = await fs.readFile('./src/views/home/index.html', { encoding: 'utf-8' });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(homeHTML);
+        break;
+      }
 
-    } else if (req.url === "/styles/site.css") {
-      const siteCss = await fs.readFile('./src/styles/site.css', { encoding: 'utf-8' });
-      res.writeHead(200, { 'Content-Type': 'text/css' });
-      res.write(siteCss);
+      case "/styles/site.css": {
+        const siteCss = await fs.readFile('./src/styles/site.css', { encoding: 'utf-8' });
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(siteCss);
+        break;
+      }
+      case "/cats/add-breed":{
+        const addBreedHTML = await fs.readFile('./src/views/addBreed.html', {encoding: 'utf-8'});
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(addBreedHTML);
+        break;
+      }
+       case "/cats/add-cat":{
+        const addCatHTML = await fs.readFile('./src/views/addCat.html', {encoding: 'utf-8'});
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(addCatHTML);
+        break;
+      }
 
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write("404 Not Found");
+      default: {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write("404 Not Found");
+      }
     }
   } catch (err) {
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.write("Server Error: " + err.message);
   }
+
   res.end();
 });
 
